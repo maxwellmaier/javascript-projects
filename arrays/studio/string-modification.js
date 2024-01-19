a0ef7fcf1ed7e71d19d71386e3755b6ae0e80647
maxwell.maier <maxwellmaier89@gmail.com>
2024-01-18 21:13:38 -0600

    finished array studio

--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -3,9 +3,23 @@ let str = "LaunchCode";
 
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
-
+let firstStr = str.substring(3,10) 
+console.log(firstStr)
+let secondStr = str.substring(0,3)
+console.log(secondStr)
 //Use a template literal to print the original and modified string in a descriptive phrase.
-
+console.log(`${str} in pig latin would be ${firstStr + secondStr}.`)
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
+let newStr = input.question("What is your name? ")
+let numToMove = input.question("How many letters would you like to move? ") || 3
 
+let newFirstStr = newStr.substring(numToMove, newStr.length)
+let newSecondStr = newStr.substring(0, numToMove)
+
+console.log(newFirstStr)
+console.log(newSecondStr)
+if (numToMove > newStr.length) {
+    console.log("Error")}
+else {
+    console.log(`${newStr} with ${numToMove} moved to the back is ${newFirstStr+newSecondStr}.`)}
 //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -1,15 +1,45 @@
 // Initialize Variables below
-
+let date = "Monday 2019-03-18"
+let time = "10:05:34 AM"
+let astronautCount = 7
+let astronautStatus = "Ready"
+let averageAstronautMassKg = 80.7
+let crewMassKg = astronautCount * averageAstronautMassKg
+let fuelMassKg = 760000
+let shuttleMassKg = 74842.31
+let totalMassKg = crewMassKg + fuelMassKg + shuttleMassKg
+let maximumMassLimit = 850000
+let fuelTempCelsius = -225
+let minimumFuelTemp = -300
+let maximumFuelTemp = -150
+let fuelLevel = "100%"
+let weatherStatus = "clear"
+let preparedForLiftOff = true 
 // add logic below to verify total number of astronauts for shuttle launch does not exceed 7
-
+if (astronautCount > 7) {
+    console.log("Do Not Launch, crew exceeds reccomended amount.")
+}
 // add logic below to verify all astronauts are ready
-
+else if (astronautStatus !== "Ready"){
+    console.log("Do Not Launch, crew not READY")
+}
 // add logic below to verify the total mass does not exceed the maximum limit of 850000
-
+else if (totalMassKg > maximumMassLimit) {
+    console.log("Do Not Launch, Total Mass exceeds Maximum Mass Limit")
+}
 // add logic below to verify the fuel temperature is within the appropriate range of -150 and -300
-
+else if (fuelTempCelsius < minimumFuelTemp || fuelTempCelsius > maximumFuelTemp) {
+    console.log("Do Not Launch Check Fuel Temperature.")
+}
 // add logic below to verify the fuel level is at 100%
-
+else if (fuelLevel !== "100%") {
+    console.log("Do Not Launch Check Fuel Levels.")
+}
 // add logic below to verify the weather status is clear
-
+else if (weatherStatus !== "clear") {
+    console.log("Do Not Launch Wait for a Clear Day")
+}
 // Verify shuttle launch can proceed based on above conditions
+else {
+    console.log ("Trip Statistics", "\n", "Date:" + date, "\n", "Time:" + time, "\n", "Astronaut Count:" + astronautCount, "\n", "Crew Mass:" + crewMassKg + "kg", "\n", "Fuel Mass:" + fuelMassKg + "kg", "\n", "Shuttle Mass:" + shuttleMassKg + "kg", "\n", "Total Mass" + totalMassKg + "kg", "\n", "Fuel Temp:" + fuelTempCelsius, "\n", "Weather Status" + weatherStatus, "\n", "Have a Safe Trip")
+}
